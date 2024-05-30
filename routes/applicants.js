c194f714167733c39e82c3426acf1b1eb0f42257
Leonardo <ldkprogramming@gmail.com>
2024-05-30 19:28:37 +0200

    corrected navbar for applicants

--- routes/applicants.js
+++ routes/applicants.js
@@ -6,22 +6,22 @@ const JobApplication = require("../models/jobApplication");
 const Recruiter = require("../models/recruiter");
 
 router.get('/:idApplicant', asyncHandler(async (req, res, next) => {
-    res.render("applicant/home", {idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId});
+    res.render("applicant/home", {idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId, idAdmin: req.session.rolesIdMap.adminId});
 }));
 
 router.get('/:idApplicant/job-offers/published', asyncHandler(async (req, res, next) => {
     const jobOffers = await JobOffer.getAllByStatusWithInfo('published');
-    res.render('applicant/offers', {jobOffers: jobOffers, idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId});
+    res.render('applicant/offers', {jobOffers: jobOffers, idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId, idAdmin: req.session.rolesIdMap.adminId});
 }));
 
 router.get('/:idApplicant/job-applications', asyncHandler(async (req, res, next) => {
     const jobApplications = await JobApplication.getNameAndTitleAndDescriptionByIdApplicant(req.params.idApplicant);
-    res.render("applicant/applications", { jobApplications: jobApplications, idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId });
+    res.render("applicant/applications", { jobApplications: jobApplications, idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId, idAdmin: req.session.rolesIdMap.adminId });
 }));
 
 router.get('/:idApplicant/become-recruiter', asyncHandler(async (req, res, next) => {
     // a faire
-    res.render("applicant/recruiter_registration_request", {idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId})
+    res.render("applicant/recruiter_registration_request", {idApplicant: req.session.rolesIdMap.applicantId, idRecruiter: req.session.rolesIdMap.recruiterId, idAdmin: req.session.rolesIdMap.adminId})
 }));
 
 router.post('/:idApplicant/become-recruiter', asyncHandler(async (req, res, next) => {
